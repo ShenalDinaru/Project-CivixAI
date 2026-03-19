090662803a8ac4042a8344329e9e936e6fb7c041
yalenthren <yalenthren2003@gmail.com>
2026-03-19 15:32:02 +0530

    icons issue sorted out

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, '../public')));
-
+app.use('/Resources', express.static(path.join(__dirname, '../Resources')));
 
 // Middleware
 app.use(cors());
